2a6cb4cfe12c0f735b1511842ca7473a22d8324e
Román Barragán Franco <al17625@cbtis051.edu.mx>
2019-11-26 11:21:41 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17625.js
+++ app_al17625.js
@@ -1,7 +1,15 @@
 var http = require('http');
 http.createServer(function(req, res){
+
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Barragán Franco Román\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
     res.end('Hola mundo Node JS Repo GIT - Barragán Franco Román\n');
+    
 }).listen(8081);
 console.log('Server running on port 8081');

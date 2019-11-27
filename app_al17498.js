60f8afc177b71cd604bb932c7795fd9ad77fb4bf
William Oman Hernandez Ortega <al17498@cbtis051.edu.mx>
2019-11-27 11:39:53 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17498.js
+++ app_al17498.js
@@ -1,7 +1,15 @@
 var http = require('http');
 http.createServer(function(req, res){
+
     res.writeHead(200,{'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola Mundo Node Js Repo Git - Hernadez Ortega William Oman\n');
+    var objeto = {
+        mensaje : 'Hola Mundo Node Repo GIT - Hernandez Ortega William Oman\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+
 }).listen(8081);
 console.log('Server running on port 8081');
